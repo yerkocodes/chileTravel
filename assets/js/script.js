4cc32c6311c9243d9b99535bf05fb7acbfabc0e6
Yerko Cortes Baeza <yerkodigo@gmail.com>
2022-04-10 18:28:14 -0400

    Added toogle method to the cards paragraph

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,17 @@ $(document).ready(function(){
     return new bootstrap.Tooltip(tooltipTriggerEl)
   })
 
-    $(window).on("scroll", function() {
-      if($(window).scrollTop() > 500) {
-        $("nav").addClass("navbar-bg-scroll");
-      } else {
-        //remove the background property so it comes transparent again (defined in your css)
-        $("nav").removeClass("navbar-bg-scroll");
-      }
-    });
+  $(window).on("scroll", function() {
+    if($(window).scrollTop() > 500) {
+      $("nav").addClass("navbar-bg-scroll");
+    } else {
+      //remove the background property so it comes transparent again (defined in your css)
+      $("nav").removeClass("navbar-bg-scroll");
+    }
+  });
+
+  $('#featured > div:nth-child(2) > article > div > .card-body > .card-title').on('click', function () {
+    $(this).parent().find('.card-text').toggle('slow');
+  });
 
 });
